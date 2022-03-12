723b61f5964483a212bf63b5080532be6edf87e6
Анастасия Васильева <99146036+vasnastia@users.noreply.github.com>
2022-03-12 14:42:55 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,12 +6,10 @@ int i = 0, j = size - 1, m = 1;
     int middle = i + (j - i) / 2;
     if (arr[middle] == value) {
       int m = middle;
-      while (arr[--middle] == value) {
+      while (arr[--middle] == value)
         m = m + 1;
-      }
-      while (arr[++m] == value) {
+      while (arr[++m] == value)
         m = m + 1;
-      }
       return m;
     } else if (arr[middle] > value) {
         j = middle;
