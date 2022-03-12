f7e95ac56c8a304e3f5f57c4e9cad237952f966b
Анастасия Васильева <99146036+vasnastia@users.noreply.github.com>
2022-03-12 13:32:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,24 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+int i = 0, j = size - 1, m = 0; //i - левая граница массива, j - правая граница массива
+  while (i < j) {
+    int middle = i + (j - i) / 2;
+    if (arr[middle] == value) {
+      int m = middle;
+      while (arr[--middle] == value) {
+        m = m + 1;
+      }
+      while (arr[++m] == value) {
+        m = m + 1;
+      }
+      return m;
+    } else if (arr[middle] > value) {
+        j = middle;
+    }
+    else {
+        i = middle + 1;
+    }
+  }
+  return 0;
 }
