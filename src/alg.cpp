327c00976a5af9702bef49b55ce485a6b6856666
Анастасия Васильева <99146036+vasnastia@users.noreply.github.com>
2022-03-12 15:56:31 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,7 +1,7 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  int i = 0, j = size - 1, k = 1;
+  int i = 0, j = size - 1, m = 1;
     while (i < j) {
       int middle = i + (j - i) / 2;
         if (arr[middle] == value) {
